69ebab27b44faffd02050bcc49ff1c96d7116b7a
nirajsai <nirajsai.challa@ikonicit.com>
2025-08-25 22:27:23 +0530

    Configuration.js updated

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -1,19 +1,19 @@
 export class AppConfiguration {
-  public static companyRestURL = 'http://localhost:8085/api/company/';
+  public static companyRestURL = 'https://gotracrat.in:8085/api/company/';
   public isProd = false;
-  public static vendorRestURL = 'http://localhost:8087/api/v1/vendor/';
-  public static vendorNoteURL = 'http://localhost:8088/api/vendor/note';
-  public static templateRestURL = 'http://localhost:8085/api/v1/template/';
-  public static typeStatusRestURL = 'http://localhost:8088/api/';
-  public static typeRestURL = 'http://localhost:8088/api/type';
+  public static vendorRestURL = 'https://gotracrat.in:8087/api/v1/vendor/';
+  public static vendorNoteURL = 'https://gotracrat.in:8088/api/vendor/note';
+  public static templateRestURL = 'https://gotracrat.in:8085/api/v1/template/';
+  public static typeStatusRestURL = 'https://gotracrat.in:8088/api/';
+  public static typeRestURL = 'https://gotracrat.in:8088/api/type';
   public static attributeRestURL =
-    'http://localhost:8088/api/attributeName';
-  public static locationRestURL = 'http://localhost:8087/api/v1/';
-  public static oauthURL = 'http://localhost:8081/oauth/token';
+    'https://gotracrat.in:8088/api/attributename';
+  public static locationRestURL = 'https://gotracrat.in:8087/api/v1/';
+  public static oauthURL = 'https://gotracrat.in:8081/oauth/token';
   public static vendorAttachments =
-    'http://localhost:8088/api/vendor/attachment';
+    'https://gotracrat.in:8088/api/vendor/attachment';
 
   public static forgotPasswordURL =
-    'http://localhost:8085/api/v1/password/';
-  public static cliffsURL = 'http://localhost:8088';
+    'https://gotracrat.in:8085/api/v1/password/';
+  public static cliffsURL = 'https://gotracrat.in:8088';
 }

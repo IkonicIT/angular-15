28e097cc99ed7df11fda50d36a0deb698936dfcd
Rajeshwar Bakaraju <155133148+rajeshwar96@users.noreply.github.com>
2026-08-13 22:36:26 +0530

    Change API URLs to new Test IP address
    Updated all REST URLs to use the new IP address.

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -1,15 +1,15 @@
 export class AppConfiguration {
-  public static companyRestURL = 'https://gotracrat.in:9090/api/company/';
+  public static companyRestURL = 'http://18.189.146.169:9090/api/company/';
   public isProd = false;
-  public static vendorRestURL = 'https://gotracrat.in:9090/api/v1/vendor/';
-  public static vendorNoteURL = 'https://gotracrat.in:9090/api/vendor/note';
-  public static templateRestURL = 'https://gotracrat.in:9090/api/v1/template/';
-  public static typeStatusRestURL = 'https://gotracrat.in:9090/api/';
-  public static typeRestURL = 'https://gotracrat.in:9090/api/type';
-  public static attributeRestURL = 'https://gotracrat.in:9090/api/attributename';
-  public static locationRestURL = 'https://gotracrat.in:9090/api/v1/';
-  public static oauthURL = 'https://gotracrat.in:9090/secured/company/login';
-  public static vendorAttachments = 'https://gotracrat.in:9090/api/vendor/attachment';
-  public static forgotPasswordURL = 'https://gotracrat.in:9090/api/v1/password/';
-  public static cliffsURL = 'https://gotracrat.in:9090';
+  public static vendorRestURL = 'http://18.189.146.169:9090/api/v1/vendor/';
+  public static vendorNoteURL = 'http://18.189.146.169:9090/api/vendor/note';
+  public static templateRestURL = 'http://18.189.146.169:9090/api/v1/template/';
+  public static typeStatusRestURL = 'http://18.189.146.169:9090/api/';
+  public static typeRestURL = 'http://18.189.146.169:9090/api/type';
+  public static attributeRestURL = 'http://18.189.146.169:9090/api/attributename';
+  public static locationRestURL = 'http://18.189.146.169:9090/api/v1/';
+  public static oauthURL = 'http://18.189.146.169:9090/secured/company/login';
+  public static vendorAttachments = 'http://18.189.146.169:9090/api/vendor/attachment';
+  public static forgotPasswordURL = 'http://18.189.146.169:9090/api/v1/password/';
+  public static cliffsURL = 'http://18.189.146.169:9090';
 }

6f12774fedc595af718d5734b07cfa5065e93b29
rajeshwar96 <rajeshwar.b@ikonicit.com>
2026-09-11 21:59:50 +0530

    changed API Urls back to angular15.gotracrat.in by adding new Target Group and opened port for 9090

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -1,15 +1,15 @@
 export class AppConfiguration {
-  public static companyRestURL = 'http://18.189.146.169:9090/api/company/';
+  public static companyRestURL = 'https://angular15.gotracrat.in:9090/api/company/';
   public isProd = false;
-  public static vendorRestURL = 'http://18.189.146.169:9090/api/v1/vendor/';
-  public static vendorNoteURL = 'http://18.189.146.169:9090/api/vendor/note';
-  public static templateRestURL = 'http://18.189.146.169:9090/api/v1/template/';
-  public static typeStatusRestURL = 'http://18.189.146.169:9090/api/';
-  public static typeRestURL = 'http://18.189.146.169:9090/api/type';
-  public static attributeRestURL = 'http://18.189.146.169:9090/api/attributename';
-  public static locationRestURL = 'http://18.189.146.169:9090/api/v1/';
-  public static oauthURL = 'http://18.189.146.169:9090/secured/company/login';
-  public static vendorAttachments = 'http://18.189.146.169:9090/api/vendor/attachment';
-  public static forgotPasswordURL = 'http://18.189.146.169:9090/api/v1/password/';
-  public static cliffsURL = 'http://18.189.146.169:9090';
+  public static vendorRestURL = 'https://angular15.gotracrat.in:9090/api/v1/vendor/';
+  public static vendorNoteURL = 'https://angular15.gotracrat.in:9090/api/vendor/note';
+  public static templateRestURL = 'https://angular15.gotracrat.in:9090/api/v1/template/';
+  public static typeStatusRestURL = 'https://angular15.gotracrat.in:9090/api/';
+  public static typeRestURL = 'https://angular15.gotracrat.in:9090/api/type';
+  public static attributeRestURL = 'https://angular15.gotracrat.in:9090/api/attributename';
+  public static locationRestURL = 'https://angular15.gotracrat.in:9090/api/v1/';
+  public static oauthURL = 'https://angular15.gotracrat.in:9090/secured/company/login';
+  public static vendorAttachments = 'https://angular15.gotracrat.in:9090/api/vendor/attachment';
+  public static forgotPasswordURL = 'https://angular15.gotracrat.in:9090/api/v1/password/';
+  public static cliffsURL = 'https://angular15.gotracrat.in:9090';
 }
